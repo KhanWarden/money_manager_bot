60a55706d73a8ac5c8281d6ac1ea884788d8fc84
KhanWarden <baryrhan2004@gmail.com>
2026-05-08 02:10:27 +0500

    Change abstract methods to async

--- bot/src/bot/domain/category/repository.py
+++ bot/src/bot/domain/category/repository.py
@@ -6,9 +6,9 @@ from bot.domain.shared import EntityId
 
 class ICategoryRepository(ABC):
     @abstractmethod
-    def get_by_name(self, category_id: EntityId) -> Category | None:
+    async def get_by_name(self, category_id: EntityId) -> Category | None:
         pass
 
     @abstractmethod
-    def save(self, name: str) -> None:
+    async def save(self, name: str) -> None:
         pass
