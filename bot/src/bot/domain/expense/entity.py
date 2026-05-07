0bd0f4669e3b4af4ae3e154643794b3d0d7f33f5
KhanWarden <baryrhan2004@gmail.com>
2026-05-08 02:09:09 +0500

    Add user_id, created_at to entity

--- bot/src/bot/domain/expense/entity.py
+++ bot/src/bot/domain/expense/entity.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 from bot.domain.shared import Money, EntityId
 
@@ -6,5 +7,7 @@ from bot.domain.shared import Money, EntityId
 @dataclass
 class Expense:
     id: EntityId
+    user_id: EntityId
     amount: Money
     category_id: EntityId
+    created_at: datetime
